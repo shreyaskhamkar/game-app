7e495b81447b4364601c489091277a6a60ef67e1
shreyaskhamkar <shreyaskhamkar96@gmail.com>
2024-03-06 15:36:24 +0530

    feat: Displaying platfrom icons

--- game-hub/src/Hooks/useGames.ts
+++ game-hub/src/Hooks/useGames.ts
@@ -2,12 +2,19 @@ import { useEffect, useState } from "react";
 import apiClient from "../Services/api-client";
 import { CanceledError } from "axios";
 
+
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: {platform: Platform}[]
   }
-  
+
   interface FetchGAmeResponse {
     count: number;
     results: Game[];
