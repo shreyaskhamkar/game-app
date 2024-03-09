7190a31bab7ff5d958010d44d82643db3027d3b7
shreyaskhamkar <shreyaskhamkar96@gmail.com>
2024-03-09 17:52:51 +0530

    feat: Showing a spinner

--- game-hub/src/Components/GenerList.tsx
+++ game-hub/src/Components/GenerList.tsx
@@ -1,9 +1,12 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGeners from "../Hooks/useGenres";
 import getCroppedImageUrl from "../Services/image-url";
 
 const GenerList = () => {
-  const { data } = useGeners();
+  const { data, isLoading, error } = useGeners();
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
     <List>
       {data.map((genre: any) => (
