4df9a2a621327066929c287592877fac7d9d7b66
shreyaskhamkar <shreyaskhamkar96@gmail.com>
2024-03-10 19:25:06 +0530

    feat: Highlight the selected genre

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -23,7 +23,10 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenerList onSelectGenre={(genre) => setSelectedGenre(genre)} />
+          <GenerList
+            selectedGenre={selectedGenre}
+            onSelectGenre={(genre) => setSelectedGenre(genre)}
+          />
         </GridItem>
       </Show>
       <GridItem area="main">
--- game-hub/src/Components/GenerList.tsx
+++ game-hub/src/Components/GenerList.tsx
@@ -11,8 +11,10 @@ import getCroppedImageUrl from "../Services/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
-const GenerList = ({ onSelectGenre }: Props) => {
+
+const GenerList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGeners();
 
   if (error) return null;
@@ -28,6 +30,7 @@ const GenerList = ({ onSelectGenre }: Props) => {
               src={getCroppedImageUrl(genre.image_background)}
             />
             <Button
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               onClick={() => onSelectGenre(genre)}
               fontSize="lg"
               variant="link"
